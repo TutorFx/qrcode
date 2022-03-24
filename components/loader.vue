7dfd20fee3df9ca65c1e0430efae47b875f8ecf5
TutorFx <gabriel_serejo@live.com>
2022-03-23 23:55:08 -0300

    Loader ADD

---
+++ components/loader.vue
@@ -0,0 +1,35 @@
+<template>
+<div v-if="overlay" style="position: absolute; height: 100vh; width: 100vw; z-index: 100;">
+    <v-row justify="center">
+        <v-sheet class="d-flex align-center justify-center" height="105vh" width="100vw">
+                <v-progress-circular
+                :size="70"
+                :width="7"
+                color="light-bg"
+                indeterminate
+                ></v-progress-circular>
+        </v-sheet>
+    </v-row>
+</div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            overlay: true
+        }
+    },
+    mounted(){
+        if (process.browser) {
+            window.onNuxtReady((app) => {
+                this.overlay = false
+            })
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,6 @@
 <template>
   <v-app>
+    <loader />
     <v-main>
       <Nuxt />
     </v-main>
